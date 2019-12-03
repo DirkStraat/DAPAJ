e094bae667dd3a1b059a61f549b86ee2919fc381
J.J. Varga <john.varga@hva.nl>
2019-12-03 14:04:15 +0100

    login scherm aangepast

--- src/main/java/nl/hava/dapaj/bankapp/controller/IndexController.java
+++ src/main/java/nl/hava/dapaj/bankapp/controller/IndexController.java
@@ -2,7 +2,6 @@ package nl.hava.dapaj.bankapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -10,7 +9,7 @@ public class IndexController {
 
     @PostMapping("go_to_login")
     public String go_to_loginHandler (Model model){
-        return "login_template";
+        return "login";
     }
 
 }
--- src/main/java/nl/hava/dapaj/bankapp/controller/LoginController.java
+++ src/main/java/nl/hava/dapaj/bankapp/controller/LoginController.java
@@ -1,6 +1,15 @@
 package nl.hava.dapaj.bankapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class LoginController {
 
+    @PostMapping ("join_dapaj")
+    public String join_dapajHandler(Model model){
+        return "join_dapaj";
+    }
 
 }
