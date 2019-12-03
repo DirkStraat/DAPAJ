0e3cd3b6c6741e9a1e8abc329f70fdf07a4cb8c3
J.J. Varga <john.varga@hva.nl>
2019-12-03 15:19:19 +0100

    link toegevoegd naar set_password

--- src/main/java/nl/hava/dapaj/bankapp/controller/LoginController.java
+++ src/main/java/nl/hava/dapaj/bankapp/controller/LoginController.java
@@ -2,6 +2,7 @@ package nl.hava.dapaj.bankapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -10,6 +11,10 @@ public class LoginController {
     @PostMapping ("join_dapaj")
     public String join_dapajHandler(Model model){
         return "join_dapaj";
+    }
+    @GetMapping ("set_password")
+    public String set_passwordHandler (Model model){
+        return "set_password";
     }
 
 }
