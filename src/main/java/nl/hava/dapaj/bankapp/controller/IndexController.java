09fe507e07628216f054ef10168c13ff47d2f612
J.J. Varga <john.varga@hva.nl>
2019-12-03 13:38:09 +0100

    index aangepast met button ipv link

--- src/main/java/nl/hava/dapaj/bankapp/controller/IndexController.java
+++ src/main/java/nl/hava/dapaj/bankapp/controller/IndexController.java
@@ -3,12 +3,14 @@ package nl.hava.dapaj.bankapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("login")
-    public String loginHandler (Model model){
-    return "login_template";
+    @PostMapping("go_to_login")
+    public String go_to_loginHandler (Model model){
+        return "login_template";
     }
+
 }
