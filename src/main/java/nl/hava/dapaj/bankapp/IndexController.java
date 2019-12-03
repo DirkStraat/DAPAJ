88d9786895857b04f76fe1648aba4b9487d2be66
J.J. Varga <john.varga@hva.nl>
2019-12-03 12:10:37 +0100

    login

---
+++ src/main/java/nl/hava/dapaj/bankapp/IndexController.java
@@ -0,0 +1,14 @@
+package nl.hava.dapaj.bankapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("login")
+    public String loginHandler (Model model){
+    return "login_template";
+    }
+}
---
+++ src/main/java/nl/hava/dapaj/bankapp/LoginController.java
@@ -0,0 +1,6 @@
+package nl.hava.dapaj.bankapp;
+
+public class LoginController {
+
+
+}
