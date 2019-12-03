5d89fc364ee4c57a9bf01e16b6a966fbbb6ca280
Dirk van der Straaten <dirk.vanderstraaten@gmail.com>
2019-12-03 16:03:45 +0100

    Adds Factory that creates n users

--- src/main/java/nl/hava/dapaj/bankapp/model/Customer.java
+++ src/main/java/nl/hava/dapaj/bankapp/model/Customer.java
@@ -1,4 +1,33 @@
 package nl.hava.dapaj.bankapp.model;
 
 public class Customer extends User {
+    private static int customerCount = 10000;
+
+
+    protected Customer(String firstName, String prefix, String lastName,
+                       String address, String postcode, String city, String country) {
+        super(firstName, prefix, lastName, address, postcode, city, country);
+        this.customerId = customerCount;
+        customerCount++;
+        this.loginName = this.creatUserName();
+
+    }
+
+    @Override
+    public String creatUserName() {
+        String part1 = this.firstName.substring(0,2);
+        String part2 = this.lastName.substring(0,2);
+        String part3 = Integer.toString(this.customerId%1000);
+
+        String userName = String.format(part1+part2+part3);
+
+        return userName;
+    }
+
+    @Override
+    public String toString() {
+        if (prefix != null){
+        return String.format(firstName +" "+prefix +" "+lastName +" "+ address+" "+postcode+" "+city+" "+loginName+" "+customerId);
+        } else return String.format(firstName +" "+lastName +" "+ address+" "+postcode+" "+city+" "+loginName+" "+customerId);
+    }
 }
--- src/main/java/nl/hava/dapaj/bankapp/model/Employee.java
+++ src/main/java/nl/hava/dapaj/bankapp/model/Employee.java
@@ -6,4 +6,16 @@ public class Employee extends User {
     private String employeePassword;
     private String role;
 
+    public Employee(String firstName, String prefix, String lastName, String address, String postcode, String city, String country, int employeeID, String employeeLoginName, String employeePassword, String role) {
+        super(firstName, prefix, lastName, address, postcode, city, country);
+        this.employeeID = employeeID;
+        this.employeeLoginName = employeeLoginName;
+        this.employeePassword = employeePassword;
+        this.role = role;
+    }
+
+    @Override
+    public String creatUserName() {
+        return null;
+    }
 }
