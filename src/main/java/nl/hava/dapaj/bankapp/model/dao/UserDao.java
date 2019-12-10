3bf893a38cfd6ecf8de312388370e08633690ad5
Dirk van der Straaten <dirk.vanderstraaten@gmail.com>
2019-12-10 09:29:18 +0100

    Makes app run

--- src/main/java/nl/hava/dapaj/bankapp/model/dao/UserDao.java
+++ src/main/java/nl/hava/dapaj/bankapp/model/dao/UserDao.java
@@ -4,6 +4,12 @@ import nl.hava.dapaj.bankapp.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserDao extends CrudRepository <User, Integer> {
+   /* User findUserByCustomerId(int customerId);
+
+    List<User> getUserList();
+*/
 }
--- src/main/java/nl/hava/dapaj/bankapp/service/UserService.java
+++ src/main/java/nl/hava/dapaj/bankapp/service/UserService.java
@@ -1,7 +1,8 @@
+/*
 package nl.hava.dapaj.bankapp.service;
 
 import nl.hava.dapaj.bankapp.model.User;
-import nl.hava.dapaj.bankapp.model.dao.UserDAO;
+import nl.hava.dapaj.bankapp.model.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,20 +11,24 @@ import java.util.List;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserDAO userDAO;
+    */
+/*//*
+/@Autowired
+    //private UserDao userDao;
 
 
     public User findUserByCustomerId(int customerId) {
-        return userDAO.findUserByCustomerId(customerId);
+        return userDao.findUserByCustomerId(customerId);
     }
 
     public List<User> getUserList() {
-        return userDAO.getUserList();
+        return userDao.getUserList();
     }
 
 
     public void save(User user) {
-        userDAO.save(user);
-    }
+        userDao.save(user);
+    }*//*
+
 }
+*/
