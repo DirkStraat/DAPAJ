a50ed791538843878d5d88072d9fa3101a53b767
Alan Lewis <alan.lewis@hva.nl>
2019-12-11 13:52:21 +0100

    welcomecustomer pagina account overzicht

--- src/main/java/nl/hava/dapaj/bankapp/service/AccountService.java
+++ src/main/java/nl/hava/dapaj/bankapp/service/AccountService.java
@@ -1,10 +1,11 @@
 package nl.hava.dapaj.bankapp.service;
 
 import nl.hava.dapaj.bankapp.model.Account;
-import nl.hava.dapaj.bankapp.model.User;
 import nl.hava.dapaj.bankapp.model.dao.AccountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountService {
@@ -12,6 +13,9 @@ public class AccountService {
     @Autowired
     private AccountDao accountDAO;
 
+    public List<Account> getAccountByUserId(int customerId) {
+        return accountDAO.getAccountByCustomersCustomerId(customerId);
+    }
 
     public void save(Account account) {
         accountDAO.save(account);
