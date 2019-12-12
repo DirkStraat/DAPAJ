cedecc12b00ff24385d2b0260368074c3d30fa41
Dirk van der Straaten <dirk.vanderstraaten@gmail.com>
2019-12-12 11:28:26 +0100

    Adds add representative & transaction constructor

--- src/main/java/nl/hava/dapaj/bankapp/controller/AccountPageController.java
+++ src/main/java/nl/hava/dapaj/bankapp/controller/AccountPageController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
@@ -19,12 +20,20 @@ public class AccountPageController {
     private AccountService accountService;
 
     @PostMapping("add_representative")
-    public String addRepresentativeHandler(Model model) {
+    public String addRepresentativeHandler(@RequestParam(name = "account_id") int id,Model model) {
+        User user = (User)model.getAttribute("user");
+        Account account = accountService.getAccountByAccountId(id);
+        model.addAttribute("user", user);
+        model.addAttribute("account", account);
         return "add_representative";
     }
 
     @PostMapping("transfer")
-    public String transferHandler(Model model) {
+    public String transferHandler(@RequestParam(name = "account_id") int id, Model model) {
+        User user = (User)model.getAttribute("user");
+        Account account = accountService.getAccountByAccountId(id);
+        model.addAttribute("user", user);
+        model.addAttribute("account", account);
         return "transfer";
     }
 
