73ca904f7330ca249df9b0f4fd4b0ae2b27cbb5a
J.J. Varga <john.varga@hva.nl>
2019-12-18 21:16:43 +0100

    redirect

--- src/main/java/nl/hava/dapaj/bankapp/controller/LoginController.java
+++ src/main/java/nl/hava/dapaj/bankapp/controller/LoginController.java
@@ -26,6 +26,7 @@ public class LoginController {
     private UserService userService;
     @Autowired
     private AccountService accountService;
+    CustomerWelcomeController customerWelcomeController;
 
 
     @PostMapping ("join_dapaj")
