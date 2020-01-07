95c5a3344401d184d4677b9eb4e0535d326762cd
J.J. Varga <john.varga@hva.nl>
2020-01-07 15:49:34 +0100

    verbetering

--- src/main/java/nl/hava/dapaj/bankapp/controller/AverageBalanceBranchController.java
+++ src/main/java/nl/hava/dapaj/bankapp/controller/AverageBalanceBranchController.java
@@ -7,6 +7,7 @@ import nl.hava.dapaj.bankapp.model.User;
 import nl.hava.dapaj.bankapp.service.SMEAccountService;
 import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
