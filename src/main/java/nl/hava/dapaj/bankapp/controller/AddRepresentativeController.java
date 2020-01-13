8454bfd11177368ce56a0cf95a358ead31fd12af
Dirk van der Straaten <dirk.vanderstraaten@gmail.com>
2020-01-13 11:35:48 +0100

    Moves addRepresentative from seperate page to account_page

--- src/main/java/nl/hava/dapaj/bankapp/controller/AddRepresentativeController.java
+++ src/main/java/nl/hava/dapaj/bankapp/controller/AddRepresentativeController.java
@@ -31,26 +31,21 @@ public class AddRepresentativeController {
     public String inviteRepresentativeHandler(@RequestParam(name = "user_name") String userName,
                                               @RequestParam(name = "keycode") String keycode , Model model) {
         User newRepresentative = userService.findUserByLoginName(userName);
-        if (newRepresentative == null){
-            model.addAttribute("motd", "Deze gebruikersnaam is niet bekend in het systeem.");
-            return "add_representative";
-        }
-        if (keycode.length() != LENGTE_KOPPELCODE){
-            model.addAttribute("motd", "Aantal tekens van koppelcode is niet juist.");
-            return "add_representative";
-        }
-
         Account account = (Account)model.getAttribute("account");
         AuthorizationInvitation invitation = authorizationInvitationService.getInvitationByUserAndAccount(newRepresentative, account);
-        if (invitation != null){
+
+        if (newRepresentative == null){
+            model.addAttribute("motd", "Deze gebruikersnaam is niet bekend in het systeem.");
+        } else if (keycode.length() != LENGTE_KOPPELCODE){
+            model.addAttribute("motd", "Aantal tekens van koppelcode is niet juist.");
+        } else if (invitation != null){
             model.addAttribute("motd", "Deze uitnodiging is al eerder naar gemachtigde verstuurd.");
-            accountPageController.enterAccountPage(account.getAccountID(), model);
         } else {
             AuthorizationInvitation authorizationInvitation = new AuthorizationInvitation(account, newRepresentative, keycode);
             authorizationInvitationService.inviteAuthorizedRepresentative(authorizationInvitation);
             model.addAttribute("motd", "Uitnodiging naar gemachtigde verstuurd. Deze kan de rekening koppelen met de koppelcode.");
-            accountPageController.enterAccountPage(account.getAccountID(), model);
         }
+        accountPageController.enterAccountPage(account.getAccountID(), model);
         return "account_page";
     }
 }
